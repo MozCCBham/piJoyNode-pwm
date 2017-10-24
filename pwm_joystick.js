53263fdc9571734e7d175524912731e08864cd75
Philip Jones <philj56@gmail.com>
2017-10-24 18:51:09 +0100

    Forgot to add actual get request

--- pwm_joystick.js
+++ pwm_joystick.js
@@ -23,11 +23,22 @@ setInterval(function(){
                 + (joystick.up()	? ' up'		: '')
                 + (joystick.left()	? ' left'	: '')
                 + (joystick.down()	? ' down' 	: '')	
-        if (joystick.up()) {
-        } else if (joystick.down()) {
-        } else if (joystick.left()) {
-        } else if (joystick.right()) {
-        } else {
+        if (str.indexOf("up") !== -1) {
+                outstr = 'forwards';
         }
+        if (str.indexOf("down") !== -1) {
+                outstr = 'backwards';
+        }
+        if (str.indexOf("right") !== -1) {
+                outstr = 'right';
+        }
+        if (str.indexOf("left") !== -1) {
+                outstr = 'left';
+        }
+        if (str === '') {
+                outstr = 'stop'
+        }
+        xhr.open('GET', outstr, true);
+        xhr.send(null)
 }, 1/30 * 1000);
 
